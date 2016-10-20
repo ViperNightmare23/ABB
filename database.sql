d7b224f6299664c45dcd5b5aaeb9c9a36d1d6e77
ViperNightmare23 <khristianp@msn.com>
2016-10-19 20:32:23 -0500

    Estructura de la base de datos

---
+++ database.sql
@@ -0,0 +1,135 @@
+-- MySQL dump 10.13  Distrib 5.7.15, for Linux (x86_64)
+--
+-- Host: localhost    Database: ABB
+-- ------------------------------------------------------
+-- Server version	5.7.13-0ubuntu0.16.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `date`
+--
+
+DROP TABLE IF EXISTS `date`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `date` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `d_id` int(11) NOT NULL,
+  `s_id` int(11) NOT NULL,
+  `last_modification` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `d_id` (`d_id`),
+  KEY `s_id` (`s_id`),
+  CONSTRAINT `date_ibfk_1` FOREIGN KEY (`d_id`) REFERENCES `device` (`id`),
+  CONSTRAINT `date_ibfk_2` FOREIGN KEY (`s_id`) REFERENCES `status` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `date`
+--
+
+LOCK TABLES `date` WRITE;
+/*!40000 ALTER TABLE `date` DISABLE KEYS */;
+/*!40000 ALTER TABLE `date` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `device`
+--
+
+DROP TABLE IF EXISTS `device`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `device` (
+  `id` int(11) NOT NULL,
+  `p_id` int(11) NOT NULL,
+  `s_id` int(11) NOT NULL,
+  `number` int(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `s_id` (`s_id`),
+  KEY `p_id` (`p_id`),
+  CONSTRAINT `device_ibfk_1` FOREIGN KEY (`s_id`) REFERENCES `status` (`id`),
+  CONSTRAINT `device_ibfk_2` FOREIGN KEY (`p_id`) REFERENCES `phase` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `device`
+--
+
+LOCK TABLES `device` WRITE;
+/*!40000 ALTER TABLE `device` DISABLE KEYS */;
+INSERT INTO `device` VALUES (1,1,1,1);
+/*!40000 ALTER TABLE `device` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `phase`
+--
+
+DROP TABLE IF EXISTS `phase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `phase` (
+  `id` int(11) NOT NULL,
+  `stage` varchar(3) NOT NULL,
+  `description` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `phase`
+--
+
+LOCK TABLES `phase` WRITE;
+/*!40000 ALTER TABLE `phase` DISABLE KEYS */;
+INSERT INTO `phase` VALUES (1,'ok','finished activity'),(2,'p','in process'),(3,'af','released with missing part'),(4,'f','process stop'),(5,'pf','in process with missing part'),(6,'na','does not apply'),(7,'rw','rework'),(8,'get','getab');
+/*!40000 ALTER TABLE `phase` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `status` (
+  `id` int(11) NOT NULL,
+  `step` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+INSERT INTO `status` VALUES (1,'wiring'),(2,'frame assembly'),(3,'internal wiring'),(4,'testing'),(5,'final shipping');
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-10-19 20:31:21
